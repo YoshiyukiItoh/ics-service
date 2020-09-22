c25ca83d49514aa7799fc8ac46bbeaa92821266c
Yoshiyuki Ito <swallows.vct1a19@gmail.com>
2020-09-22 15:32:25 +0900

    deleted unneccesary line.

--- ics_test.go
+++ ics_test.go
@@ -8,11 +8,9 @@ func TestIsLeapYear(t *testing.T) {
 	if !isLeapYear(2000) {
 		t.Errorf("2000 was expected to be Leap year.")
 	}
-
 	if !isLeapYear(2004) {
 		t.Errorf("2000 was expected to be Leap year.")
 	}
-
 	if isLeapYear(2001) {
 		t.Errorf("2001 was not expected to be Leap year.")
 	}
