6628a922fb830a1cd4fc9d7447f7cdd82b00d648
Yoshiyuki Ito <yoshiyuki.ito.biz@gmail.com>
2020-09-23 23:17:34 +0900

    add test case of getDaysOfMonth function.

--- ics_test.go
+++ ics_test.go
@@ -4,6 +4,18 @@ import (
 	"testing"
 )
 
+func TestGetDaysOfMonth(t *testing.T) {
+	actualLeapYear := [12]int{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+	if actualLeapYear != getDaysOfMonth(2000) {
+		t.Errorf("expected return an array of leap year.")
+	}
+
+	actualNotLeapYear := [12]int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+	if actualNotLeapYear != getDaysOfMonth(2001) {
+		t.Errorf("expected return an array of not leap year.")
+	}
+}
+
 func TestIsLeapYear(t *testing.T) {
 	if !isLeapYear(2000) {
 		t.Errorf("2000 was expected to be Leap year.")
